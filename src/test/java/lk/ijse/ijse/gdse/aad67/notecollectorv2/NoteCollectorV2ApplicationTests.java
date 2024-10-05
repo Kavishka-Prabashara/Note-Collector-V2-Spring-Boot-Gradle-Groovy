6a8065231936dbedd3e9375056f8445b1a3301bb
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2024-10-05 12:07:39 +0530

    initial commit

---
+++ src/test/java/lk/ijse/ijse/gdse/aad67/notecollectorv2/NoteCollectorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ijse.gdse.aad67.notecollectorv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteCollectorV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
